5551688b400f04deddaca7c6c0808b77f4455223
𝘿𝙊𝙉𝘼𝙏𝙀 ๛ 𝘼𝙍𝙈𝙔 🇮🇳 ™ <sanjoymandal.902137@gmail.com>
2024-09-01 16:52:38 +0530

    Add files via upload

---
+++ plugins/autobroadcast.py
@@ -0,0 +1,101 @@
+import asyncio
+
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+
+from config import AUTO_GCAST, AUTO_GCAST_MSG, LOGGER_ID
+from DONATE_ARMY_MUSIC import app
+from DONATE_ARMY_MUSIC.utils.database import get_served_chats
+
+# Convert AUTO_GCAST to boolean based on "On" or "Off"
+AUTO_GCASTS = AUTO_GCAST.strip().lower() == "on"
+
+START_IMG_URLS = "https://telegra.ph/file/a1ee9c24963567afc3907.jpg"
+
+MESSAGES = f"""**☄️ᴊᴏɪɴ ᴏᴜʀ ᴛᴇʟᴇɢʀᴀᴍ ᴄʜᴀɴɴᴇʟ 🔗:- https://t.me/+TEOLVuOuPl01Y2U1
+
+ᴛᴇʟᴇɢʀᴀᴍ ᴄʜᴇᴀᴛ ɢʀᴏᴜᴘ :- https://t.me/+_kBI4ZaZA3ViZWU1
+
+🌟𝙰𝙳𝙳 𝙼𝙴 𝙸𝙽 𝚈𝙾𝚄𝚁 𝙶𝚁𝙾𝚄𝙿 𝙰𝙽𝙳 𝙴𝙽𝙹𝙾𝚈💓:-  @DONATE_ARMY_x_MUSIC_BOT**"""
+
+
+BUTTONS = InlineKeyboardMarkup(
+    [
+        [
+            InlineKeyboardButton("𝖬𝖸 𝖢𝖴𝖳𝖤 🥰 𝖮𝖶𝖭𝖤𝖱 𝖢𝖮𝖭𝖳𝖠𝖢𝖳 𝖧𝖨𝖬💋", url=f"https://t.me/DONATE_OWNER_BOT")
+        ]
+    ]
+)
+
+MESSAGE = f"""**๏ ᴛʜɪs ɪs ᴀᴅᴠᴀɴᴄᴇᴅ ᴍᴜsɪᴄ ᴘʟᴀʏᴇʀ ʙᴏᴛ ғᴏʀ ᴛᴇʟᴇɢʀᴀᴍ ɢʀᴏᴜᴘs + ᴄʜᴀɴɴᴇʟs ᴠᴄ. 💌
+
+🎧 ᴘʟᴀʏ + ᴠᴘʟᴀʏ + ᴄᴘʟᴀʏ 🎧
+
+➥ sᴜᴘᴘᴏʀᴛᴇᴅ ᴡᴇʟᴄᴏᴍᴇ - ʟᴇғᴛ ɴᴏᴛɪᴄᴇ, ᴛᴀɢᴀʟʟ, ᴠᴄᴛᴀɢ, ʙᴀɴ - ᴍᴜᴛᴇ, sʜᴀʏʀɪ, ʟᴜʀɪᴄs, sᴏɴɢ - ᴠɪᴅᴇᴏ ᴅᴏᴡɴʟᴏᴀᴅ, ᴇᴛᴄ... ❤️
+
+🔐ᴜꜱᴇ » [/start](https://t.me/{app.username}?start=help) ᴛᴏ ᴄʜᴇᴄᴋ ʙᴏᴛ
+
+➲ ʙᴏᴛ :** @{app.username}"""
+
+BUTTON = InlineKeyboardMarkup(
+    [
+        [
+            InlineKeyboardButton(
+                "๏ ᴋɪᴅɴᴀᴘ ᴍᴇ ๏",
+                url=f"https://t.me/DONATE_ARMY_x_MUSIC_BOT?startgroup=s&admin=delete_messages+manage_video_chats+pin_messages+invite_users",
+            )
+        ]
+    ]
+)
+
+caption = f"""{AUTO_GCAST_MSG}""" if AUTO_GCAST_MSG else MESSAGES
+
+TEXT = """**ᴀᴜᴛᴏ ɢᴄᴀsᴛ ɪs ᴇɴᴀʙʟᴇᴅ sᴏ ᴀᴜᴛᴏ ɢᴄᴀsᴛ/ʙʀᴏᴀᴅᴄᴀsᴛ ɪs ᴅᴏɪɴɢ ɪɴ ᴀʟʟ ᴄʜᴀᴛs ᴄᴏɴᴛɪɴᴜᴏᴜsʟʏ.**\n**ɪᴛ ᴄᴀɴ ʙᴇ sᴛᴏᴘᴘᴇᴅ ʙʏ ᴘᴜᴛ ᴠᴀʀɪᴀʙʟᴇ [ᴀᴜᴛᴏ_ɢᴄᴀsᴛ = (Off)]**"""
+
+
+async def send_text_once():
+    try:
+        await app.send_message(LOGGER_ID, TEXT)
+    except Exception as e:
+        pass
+
+
+async def send_message_to_chats():
+    try:
+        chats = await get_served_chats()
+
+        for chat_info in chats:
+            chat_id = chat_info.get("chat_id")
+            if isinstance(chat_id, int):  # Check if chat_id is an integer
+                try:
+                    await app.send_photo(
+                        chat_id,
+                        photo=START_IMG_URLS,
+                        caption=caption,
+                        reply_markup=BUTTONS,
+                    )
+                    await asyncio.sleep(
+                        20
+                    )  # Sleep for 20 seconds between sending messages
+                except Exception as e:
+                    pass  # Do nothing if an error occurs while sending message
+    except Exception as e:
+        pass  # Do nothing if an error occurs while fetching served chats
+
+
+async def continuous_broadcast():
+    await send_text_once()  # Send TEXT once when bot starts
+
+    while True:
+        if AUTO_GCASTS:
+            try:
+                await send_message_to_chats()
+            except Exception as e:
+                pass
+
+        # Wait for 100000 seconds before next broadcast
+        await asyncio.sleep(100000)
+
+
+# Start the continuous broadcast loop if AUTO_GCASTS is True
+if AUTO_GCASTS:
+    asyncio.create_task(continuous_broadcast())
